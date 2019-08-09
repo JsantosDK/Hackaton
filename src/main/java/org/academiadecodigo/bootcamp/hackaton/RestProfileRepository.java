db661f8916da1e363a70c78768611cdbcd235a5d
Julio <julio.massano@gmail.com>
2019-08-09 04:41:31 +0100

    Refactor

--- src/main/java/org/academiadecodigo/bootcamp/hackaton/RestProfileRepository.java
+++
@@ -1,8 +0,0 @@
-package org.academiadecodigo.bootcamp.hackaton;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-public interface RestProfileRepository extends CrudRepository<Profile, Integer> {
-}
